38d52002800713d7c14394053d8f095de6c858c2
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-11-17 11:56:24 +0000

    my express back end

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 //my express server
 const express = require("express");
+const cors = require('cors');
+const postsRouter = require("./routes/posts"); //imported my new router
 const app = express();
 const PORT = 5001;
+app.use(cors()); //the cors middleware is used here 
+app.use(express.json()); //middleware - for my server to read json from req body
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from the Blogify server!" });
-});
+app.use("/api/posts", postsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+//npm run dev is like saying npm run vite since it is defined by the developer of vite tool.
